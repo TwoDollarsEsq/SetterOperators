2e7609724c1761680c8ee4fd809576b6072a0cf2
Artyom Rudakov <two_dollars@icloud.com>
2020-06-13 19:10:47 +0300

    Adds a few simple performance tests

--- Tests/SetterOperatorsTests/SetterOperatorsTests.swift
+++ Tests/SetterOperatorsTests/SetterOperatorsTests.swift
@@ -56,6 +56,63 @@ final class SetterOperatorsTests: XCTestCase {
         // Then
         XCTAssert(person.name == person.name && result.age == result.age)
     }
+    
+    class Value {
+        init() {}
+        var a: Int = 0
+        var b: Int = 0
+        var c: Int = 0
+        var d: Int = 0
+        var e: Int = 0
+        var f: Int = 0
+        var g: Int = 0
+        var h: Int = 0
+    }
+    
+    let N = 100000
+    
+    func testRawPerformance() {
+        self.measure {
+            @discardableResult
+            func getValue() -> Value {
+                let value = Value()
+                value.a = 42
+                value.b = 42
+                value.c = 42
+                value.d = 42
+                value.e = 42
+                value.f = 42
+                value.g = 42
+                value.h = 42
+                return value
+            }
+            
+            for _ in 0...N {
+                getValue()
+            }
+        }
+    }
+    
+    func testOperatorPerformance() {
+        self.measure {
+            @discardableResult
+            func getValue() -> Value {
+                .init()
+                    .. \.a .= 42
+                    .. \.b .= 42
+                    .. \.c .= 42
+                    .. \.d .= 42
+                    .. \.e .= 42
+                    .. \.f .= 42
+                    .. \.g .= 42
+                    .. \.h .= 42
+            }
+            
+            for _ in 0...N {
+                getValue()
+            }
+        }
+    }
 
     static var allTests = [
         ("testStructUpdating", testStructUpdating),
