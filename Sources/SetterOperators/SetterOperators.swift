bb36a049827270ae52177f54b0a9ed056185edb4
Artyom Rudakov <two_dollars@icloud.com>
2020-06-13 14:37:06 +0300

    Adds documentation

--- Sources/SetterOperators/SetterOperators.swift
+++ Sources/SetterOperators/SetterOperators.swift
@@ -5,6 +5,11 @@ precedencegroup infixl0 {
 infix operator ..: infixl0
 infix operator .=: infixl0
 
+/// Dives into instance using provided keyPath and creates a setter that expects value to set.
+/// - Warning: This overload is for value-types and it creates a copy of an instance for each setting.
+/// - Parameter root: Instance to create a setter for.
+/// - Parameter keyPath: Specifies a path that should be updated in setter.
+/// - Returns: A setter-closure that expects a value and returns updated instance.
 public func ..<Root, Value>(
     root: Root,
     keyPath: WritableKeyPath<Root, Value>
@@ -16,6 +21,11 @@ public func ..<Root, Value>(
     }
 }
 
+/// Dives into instance using provided keyPath and creates a setter that expects value to set.
+/// - Warning: This overload is for reference-types so it updates values in place capturing root object.
+/// - Parameter root: Object to create a setter for.
+/// - Parameter keyPath: Specifies a path that should be updated in setter.
+/// - Returns: A setter-closure that expects a value and returns updated object.
 public func ..<Root: AnyObject, Value>(
     root: Root,
     keyPath: ReferenceWritableKeyPath<Root, Value>
@@ -26,6 +36,10 @@ public func ..<Root: AnyObject, Value>(
     }
 }
 
+/// Combines a setter-closure with a new value to set.
+/// - Parameter setter: A setter-closure that expects for parameter.
+/// - Parameter value: A new value that should be set with setter-closer.
+/// - Returns: Updated `Root` instance.
 public func .=<Root, Value>(setter: (Value) -> Root, value: Value) -> Root {
     setter(value)
 }
